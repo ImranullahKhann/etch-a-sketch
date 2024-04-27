9e9b2c088b9494e88ad04b94e4490e243ea85ba5
Imranullah Khan <129750188+ImranullahKhann@users.noreply.github.com>
2024-04-27 10:20:14 +0500

    Feat: Made Etch-a-Sketch

---
+++ app.js
@@ -0,0 +1,46 @@
+const container = document.querySelector(".container");
+
+const btn = document.querySelector("button");
+let height = 0;
+let width = 0;
+btn.addEventListener("click", () => {
+  // if there are already squares remove them
+  if (height !== 0 && width !== 0)
+  {
+    squares = document.querySelectorAll(".square");
+    for (i = 0; i < height*width; i++)
+    {
+      squares.forEach((sqr) => {
+        sqr.remove();
+      });
+    }
+  }
+  height = +prompt("Height: ");
+  width = +prompt("Width: ");
+  // making squares and appending them to container
+  for (i = 0; i < height*width; i++)
+  {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+  }
+  // if there are any squares
+  if (height !== 0 && width !== 0)
+  {
+    squares = document.querySelectorAll(".square");
+    // making squares black when mouses goes over them and
+    //Giving the squares the right height and width
+    squares.forEach((sqr) => {
+      sqr.style.cssText = `min-height: ${100 / height}%; min-width: ${100 / width}%;`
+      sqr.addEventListener("mouseover", () => {
+        sqr.style.backgroundColor = "black";
+      });
+  });
+  }
+})
+
+
+
+
+
+
